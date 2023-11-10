2bdcc2fda6ffc69eac647535ea11c6d94818409f
Joao <joaopedrolimadias102@gmail.com>
2023-11-10 19:20:25 +0000

    Comentario2

--- app/src/main/java/com/example/practica2_joaopedrolimadias/VentanaCinco/VentanaCinco.kt
+++ app/src/main/java/com/example/practica2_joaopedrolimadias/VentanaCinco/VentanaCinco.kt
@@ -7,9 +7,11 @@ import java.io.BufferedWriter
 import java.io.File
 import java.io.FileWriter
 import android.os.Bundle
+import android.widget.RadioGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
@@ -26,22 +28,23 @@ import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.selects.select
+import java.io.BufferedReader
+import java.io.FileReader
 
 class VentanaCinco {
     @OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
     @SuppressLint("NotConstructor")
     @Composable
-    fun VentanaCinco(context: Context){
+    fun VentanaCinco(context: Context) : String{
 
         var textFieldValue by remember { mutableStateOf("") }
         var resultText by remember { mutableStateOf("") }
-
         var keyboardController by remember { mutableStateOf<SoftwareKeyboardController?>(null) }
-
+        var valor by remember { mutableStateOf("") }
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
+                .fillMaxWidth()
         ) {
             TextField(
                 value = textFieldValue,
@@ -57,31 +60,126 @@ class VentanaCinco {
                 ),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp)
             )
-            Button(
-                onClick = {
-                    resultText = textFieldValue
-                    createFile(context,"Num",textFieldValue)
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(56.dp)
-            ) {
-                Text("Obtener texto")
-            }
-            Text("Texto obtenido: $resultText", color = Color.Gray)
+
         }
+        valor = textFieldValue
+        return valor
     }
-        fun createFile(context: Context, fileName: String, content: String) {
-            try {
-                val rutaFichero = context.filesDir
-                val ficheroNuevo = File(rutaFichero, fileName)
-                val bw = BufferedWriter(FileWriter(ficheroNuevo))
-                bw.write(content)
-                bw.close()
-            } catch (e: Exception) {
-                e.printStackTrace()
+
+}
+@Composable
+fun RadioBoton() : String {
+    var selectedValue by remember { mutableStateOf("True") }
+    var valor by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            RadioButton(
+                selected = selectedValue == "True",
+                onClick = { selectedValue = "True" },
+                modifier = Modifier
+                    .selectable(
+                        selected = selectedValue == "True",
+                        onClick = { selectedValue = "True" }
+                    )
+            )
+            Text("True")
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            RadioButton(
+                selected = selectedValue == "False",
+                onClick = { selectedValue = "False" },
+                modifier = Modifier
+                    .selectable(
+                        selected = selectedValue == "False",
+                        onClick = { selectedValue = "False" }
+                    )
+            )
+            Text("False")
+
+        }
+      //  Button(
+       //     onClick = {
+//                createFile(context,"Archivo", "$valor\n$selectedValue ")
+//            },
+       //     modifier = Modifier
+      //          .fillMaxWidth()
+       //         .height(50.dp)
+      //  ) {
+       //     Text("Poner")}
+    }
+    return selectedValue
+}
+
+fun createFile(context: Context, fileName: String, content: String) {
+    try {
+        val rutaFichero = context.filesDir
+        val fichero = File(rutaFichero, fileName)
+
+        if (fichero.exists()) {
+            // Si el archivo ya existe, añadir el nuevo contenido al final
+            val bw = BufferedWriter(FileWriter(fichero, true))
+            bw.write(content)
+            bw.newLine()
+            bw.close()
+        } else {
+            // Si el archivo no existe, crearlo con el nuevo contenido
+            val bw = BufferedWriter(FileWriter(fichero))
+            bw.write(content)
+            bw.close()
+        }
+
+        readAndProcessFile(context, fileName)
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
+private fun readAndProcessFile(context: Context, fileName: String) {
+    try {
+        val rutaFichero = context.filesDir
+        val fichero = File(rutaFichero, fileName)
+        var lineCount = 0
+
+        BufferedReader(FileReader(fichero)).use { reader ->
+            var line1: String? = reader.readLine()
+            var line2: String?
+
+            while (line1 != null) {
+                line2 = reader.readLine()
+
+                if (line2 != null) {
+                    println("$line1")
+                    println("$line2")
+                } else {
+                    // Si el número de líneas en el archivo es impar,
+                    // puedes manejar la última línea de manera diferente
+                    println("$line2")
+                }
+
+                line1 = reader.readLine()
+                lineCount += 2
             }
         }
+
+        println("Se procesaron $lineCount líneas.")
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+@Composable
+fun Buton(context: Context){
+    var Boton  = RadioBoton()
+    var Num = VentanaCinco()
+    Button(onClick =  { createFile(context, "Archivo", "e") }, Text( "vamos"))
+
 }
